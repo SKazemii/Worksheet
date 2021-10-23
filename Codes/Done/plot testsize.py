a816762a68a301f4860e12c7d2647d675b664499
Saeed Kazemi <Kazemi_saeed@hotmail.com>
2021-10-23 12:30:32 -0300

    got results of WS1

--- Codes/Done/plot testsize.py
+++ Codes/Done/plot testsize.py
@@ -17,6 +17,7 @@ from scipy.stats import shapiro, ttest_ind, mannwhitneyu
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from MLPackage import util as perf
 from MLPackage import FS 
+from MLPackage import stat 
 
 
 plt.rcParams["font.size"] = 13
@@ -59,9 +60,14 @@ Results_DF["Feature_Type"] = Results_DF["Feature_Type"].map(lambda x: "COAs-otsu
 Results_DF["Feature_Type"] = Results_DF["Feature_Type"].map(lambda x: "COAs-simple" if x == "COAs_simple" else x)
 
 
-# for mode in ["Correlation", "Euclidean distance"]:
-#     for criteria in perf.model_types:
-        
+pd.set_option('display.max_rows', 100)
+Results_DF["mean_acc"] = (Results_DF["Mean_Acc_L"] + Results_DF["Mean_Acc_R"])/2
+Results_DF["mean_eer"] = (Results_DF["Mean_EER_L_te"] + Results_DF["Mean_EER_R_te"])/2
+print(stat.stat(Results_DF[["Criteria", "mean_acc"]], labels=["Criteria", "mean_acc"], plot = False).head(100))
+print(stat.stat(Results_DF[["Criteria", "mean_eer"]], labels=["Criteria", "mean_eer"], plot = False).head(100))
+# plt.show()
+
+1/0       
 plt.figure(figsize=(14,8))
 Results_DF_group = Results_DF.groupby(["Test_Size"])
 values = Results_DF["Test_Size"].sort_values().unique()
@@ -72,23 +78,28 @@ FRR_L = list()
 FAR_R = list()
 FRR_R = list()        
 for value in values:
+
+
     
     DF = Results_DF_group.get_group((value))
     X.loc[value, "Accuracy"] = "{:2.2f} +/- {:2.2f} ({:.2f}, {:.2f})".format(
-        (DF["Mean_Acc_L"].mean()+DF["Mean_Acc_R"].mean())/2, 
-        (DF["Mean_Acc_L"].std()+ DF["Mean_Acc_R"].min())/2,
-        (DF["Mean_Acc_L"].min()+ DF["Mean_Acc_R"].min())/2, 
-        (DF["Mean_Acc_L"].max()+ DF["Mean_Acc_R"].max())/2)
+        (DF["Mean_Acc_L"].mean()+ DF["Mean_Acc_R"].mean())/2, 
+        (DF["Mean_Acc_L"].std() + DF["Mean_Acc_R"].min())/2,
+        (DF["Mean_Acc_L"].min() + DF["Mean_Acc_R"].min())/2, 
+        (DF["Mean_Acc_L"].max() + DF["Mean_Acc_R"].max())/2)
     X.loc[value, "EER"] = "{:2.2f} +/- {:2.2f} ({:.2f}, {:.2f})".format(
-        (DF["Mean_EER_L_te"].mean()+DF["Mean_EER_R_te"].mean())/2,  
-        (DF["Mean_EER_L_te"].std()+ DF["Mean_EER_R_te"].min())/2,
-        (DF["Mean_EER_L_te"].min()+ DF["Mean_EER_R_te"].min())/2, 
-        (DF["Mean_EER_L_te"].max()+ DF["Mean_EER_R_te"].max())/2)
+        (DF["Mean_EER_L_te"].mean()+ DF["Mean_EER_R_te"].mean())/2,  
+        (DF["Mean_EER_L_te"].std() + DF["Mean_EER_R_te"].min())/2,
+        (DF["Mean_EER_L_te"].min() + DF["Mean_EER_R_te"].min())/2, 
+        (DF["Mean_EER_L_te"].max() + DF["Mean_EER_R_te"].max())/2)
     X.loc[value, "F1-score"] = "{:2.2f} +/- {:2.2f} ({:.2f}, {:.2f})".format(
-        (DF["Mean_f1_L"].mean()+DF["Mean_f1_R"].mean())/2,  
-        (DF["Mean_f1_L"].std()+ DF["Mean_f1_R"].min())/2,
-        (DF["Mean_f1_L"].min()+ DF["Mean_f1_R"].min())/2, 
-        (DF["Mean_f1_L"].max()+ DF["Mean_f1_R"].max())/2)
+        (DF["Mean_f1_L"].mean()+ DF["Mean_f1_R"].mean())/2,  
+        (DF["Mean_f1_L"].std() + DF["Mean_f1_R"].min())/2,
+        (DF["Mean_f1_L"].min() + DF["Mean_f1_R"].min())/2, 
+        (DF["Mean_f1_L"].max() + DF["Mean_f1_R"].max())/2)
+
+
+    
 
 
     FAR_L.append(DF[["FAR_L_" + str(i) for i in range(100)]].mean().values)
